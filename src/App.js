b76b9639643be246a7f279c00e5741297bf60ac3
jnissenbaum18 <jnissenbaum@verizon.net>
2022-08-07 20:26:44 -0400

    Refactored Auth to use context API, added admin route check

--- src/App.js
+++ src/App.js
@@ -1,56 +1,46 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import RegistrationPage from "./Pages/RegistrationPage";
 import LoginPage from "./Pages/LoginPage";
 import NavBar from "./Components/NavBar";
 
+import { useAuth } from "./Hooks/Auth";
+
 import "./App.css";
 
-function App() {
-  const [isAuthLoading, setIsAuthLoading] = useState(false);
+const AdminLayout = () => {
   const [isAdmin, setIsAdmin] = useState(false);
+  const { verifyAdmin } = useAuth();
 
+  useEffect(() => {
+    const isAdminCheck = async () => {
+      const isAdmin = await verifyAdmin();
+      setIsAdmin(isAdmin);
+    };
+    isAdminCheck();
+  }, []); // This useEffect will trigger once when the user tries to visit /admin
+
+  return (
+    <div>
+      {!isAdmin && <h3>You Must Be An Admin To View This Page. Sorry.</h3>}
+      {isAdmin && <Outlet />}
+    </div>
+  );
+};
+
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <NavBar
-                isAuthLoading={isAuthLoading}
-                setIsAuthLoading={setIsAuthLoading}
-              />
-            }
-          >
+          <Route path="/" element={<NavBar />}>
             <Route index element={<HomePage />} />
-            <Route
-              path="login"
-              element={
-                <LoginPage
-                  isAuthLoading={isAuthLoading}
-                  setIsAuthLoading={setIsAuthLoading}
-                />
-              }
-            />
-            
-            {isAdmin && <Route
-              path="registration"
-              element={
-                <h1>Admin Page</h1>
-              }
-            />}
-
-            <Route
-              path="registration"
-              element={
-                <RegistrationPage
-                  isAuthLoading={isAuthLoading}
-                  setIsAuthLoading={setIsAuthLoading}
-                />
-              }
-            />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="registration" element={<RegistrationPage />} />
+            <Route path="admin" element={<AdminLayout />}>
+              <Route index element={<h1>Admin Page</h1>} />
+            </Route>
           </Route>
         </Routes>
       </header>
--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,35 +1,42 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { loginUser, validateToken } from "../Auth";
+import { useAuth } from "../Hooks/Auth";
 
 const LoginPage = (props) => {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const Navigate = useNavigate()
-    return (
-      <div>
-        <h1>Login Page</h1>
-        <label>Username</label>
-        <input type="text" onChange={(e)=>{
-            setUsername(e.target.value)
-        }}/>
-        <label>Password</label>
-        <input type="password" onChange={(e)=>{
-            setPassword(e.target.value)
-        }}/>
-        <button onClick={async ()=>{
-            props.setIsAuthLoading(true)
-            const loginResult = await loginUser(username, password)
-            if (loginResult) {
-                props.setIsAuthLoading(false)
-            }
-        }}>Login</button>
-        <button onClick={async ()=>{
-            await validateToken()
-        }}>Validate</button>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginMessage, setLoginMessage] = useState("");
+  const { login } = useAuth();
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <h3>{loginMessage}</h3>
+      <label>Username</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <label>Password</label>
+      <input
+        type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button
+        onClick={async () => {
+          const loginResult = await login(username, password);
+          if (!loginResult.success) {
+            setLoginMessage(loginResult.message)
+          }
+        }}
+      >
+        Login
+      </button>
+    </div>
+  );
+};
 
-      </div>
-    );
-  };
-  
-  export default LoginPage;
+export default LoginPage;
